3147b90446bbb6a1a6beb9930ac0c664b7279ce8
David Fang <13846968+davidfang00@users.noreply.github.com>
2022-11-22 00:58:32 -0500

    improve docs

--- src/dijkstra.jl
+++ src/dijkstra.jl
@@ -1,20 +1,21 @@
 """
     dijkstra(g, s)
-Returns the shortest path (in terms of time) from the source `s` to all other points in the graph `g` and the parents to reconstruct the shortest path.
+Finds the shortest path (in terms of time) from the source to all other points in the graph `g` and the parents list to reconstruct the shortest path.
+Returns a tuple (shortest_path, parents).
 # Parameters
 - `g`: The city as a [`AdjacencyGraph`](@ref).
-- 's': The source junction index.
+- `s`: The source junction index.
 """
 function dijkstra(g, s)
-    dist = fill(Inf, nb_vertices(g))  # here
+    dist = fill(Inf, nb_vertices(g))
     parents = fill(9999999, nb_vertices(g))
 
     queue = PriorityQueue{Int,Float64}()
     enqueue!(queue, s => 0.0)
     while !isempty(queue)
         u, dist[u] = dequeue_pair!(queue)
-        for v in outneighbors(g, u)  # here
-            dist_v = dist[u] + edge_time(g, u, v)  # here
+        for v in outneighbors(g, u)
+            dist_v = dist[u] + edge_time(g, u, v)  # weight by time
             if dist_v < dist[v]
                 dist[v] = dist_v
                 queue[v] = dist_v
@@ -30,9 +31,9 @@ end
 Returns the shortest path (in terms of time) from the source `s` to the junction `x`.
 # Parameters
 - `x`: The target junction index.
-- 'dists': The shortest paths in terms of time.
-- 'source': The source junction index.
-- 'parents': The parents array.
+- `dists`: The shortest paths in terms of time.
+- `source`: The source junction index.
+- `parents`: The parents array.
 """
 function spath(x, dists, source, parents)
     return x == source ? x : [spath(parents[x], dists, source, parents) x]
