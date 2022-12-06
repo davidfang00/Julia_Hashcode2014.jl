ec08b9560c728c43213942b70572cf703e340665
David Fang <13846968+davidfang00@users.noreply.github.com>
2022-12-05 23:11:54 -0500

    add docs

--- src/metrics.jl
+++ src/metrics.jl
@@ -1,3 +1,9 @@
+"""
+    node_metric(node)
+Returns the metric for the current node. Currently uses distance/time as the metric.
+# Parameters
+- `node::TreeNode`: the node in the BFS lookahead tree.
+"""
 function node_metric(node::TreeNode)
     m = node.distance_traveled / (node.time_elapsed)
     return m
